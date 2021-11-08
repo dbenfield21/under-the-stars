6404c2819b5967ad2c3d57db71199fd25f30d29d
Darby Benfield <thinxing@hotmail.com>
2021-11-07 21:38:37 -0700

    campsite schema

---
+++ models/campsite.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+const campsiteSchema = new mongoose.Schema({
+  name: String,
+  location: String,
+  rating: Number,
+  camped: Boolean, 
+  comments: [{type: Schema.Types.ObjectId, ref: "Comments"}],
+  savedBy: [{type: Schema.Types.ObjectId, ref: "Profile"}],
+}, {
+  timestamps: true
+})
+
+const Campsite = mongoose.model('Campsite', campsiteSchema)
+
+export {
+  Campsite
+}
