4f2971cdcaeec290e81d1ee8efe499349e99bc86
Darby Benfield <thinxing@hotmail.com>
2021-11-07 21:34:40 -0700

    profile schema

--- models/profile.js
+++ models/profile.js
@@ -3,6 +3,10 @@ import mongoose from 'mongoose'
 const profileSchema = new mongoose.Schema({
   name: String,
   avatar: String,
+  bio: String, 
+  comments: [{type: Schema.Types.ObjectId, ref: "Comments"}],
+  following: [{type: Schema.Types.ObjectId, ref: "Profile"}],
+  favoriteSite: [{type: Schema.Types.ObjectId, ref: "Campsites"}]
 }, {
   timestamps: true
 })
---
+++ routes/profiles.js
@@ -0,0 +1,17 @@
+import { Router } from 'express'
+
+const router = Router()
+
+
+function isLoggedIn (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next ()
+  
+  }
+  res.redirect('/auth/google')
+}
+
+
+export {
+  router
+}
